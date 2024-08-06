9278af71e30bdaa36cf4733fb78983a1723f7f0e
Makdum Ibrohim <makdumibrohim3@gmail.com>
2024-08-06 22:16:12 +0700

    menambahkan inputan

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -7,5 +7,13 @@ int main()
     int pertama, kedua;
     char operation;
 
+    cout << "Masukkan angka pertama: ";
+    cin >> pertama;
+    cout << "Masukkan angka kedua: ";
+    cin >> kedua;
+    cout << "Masukkan operasi: ";
+    cin >> operation;
+
+    
     return 0;
 }
