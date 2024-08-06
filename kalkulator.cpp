a56c160d4fc026ddc28495786180b4e6747ba8bb
Makdum Ibrohim <makdumibrohim3@gmail.com>
2024-08-06 22:52:17 +0700

    1. menambahkan fungsi result yang betype data float dan membuat kondisi validasi.\n2. mengubah variable dengan type data float

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -2,18 +2,53 @@
 
 using namespace std;
 
+float result(float pertama, float kedua, char operation)
+{
+    switch (operation)
+    {
+    case '+':
+        return pertama + kedua;
+    case '-':
+        return pertama - kedua;
+    case '*':
+        return pertama * kedua;
+    case '/':
+        if (kedua == 0)
+        {
+            cerr << "Error: Cannot divide by zero.\n";
+            return 1;
+        }
+        else
+        {
+            return pertama / kedua;
+        }
+    default:
+        cerr << "Opsi tidak tersedia\n";
+        return 1;
+    }
+}
+
 int main()
 {
-    int pertama, kedua;
+    float pertama, kedua;
     char operation;
 
     cout << "Masukkan angka pertama: ";
     cin >> pertama;
-    cout << "Masukkan angka kedua: ";
-    cin >> kedua;
     cout << "Masukkan operasi: ";
     cin >> operation;
+    cout << "Masukkan angka kedua: ";
+    cin >> kedua;
 
-    
+    float hasil = result(pertama, kedua, operation);
+    if (hasil == 1 && operation == '/')
+    {
+        cout << "Hasil: " << hasil << endl;
+    }
+    else
+    {
+        cout << "Hasil: " << hasil << endl;
+    }
+
     return 0;
 }
